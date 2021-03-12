e269d2b3701e619b920b57e64e9e3ed54bdade64
Jakub Sokołowski <jakub@status.im>
2021-03-12 16:37:57 +0100

    examples/task_decorator.py: task dependencies example
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

---
+++ examples/task_decorator.py
@@ -0,0 +1,26 @@
+from airflow import DAG
+from airflow.utils.dates import days_ago
+from airflow.operators.dummy import DummyOperator
+
+DEFAULT_ARGS = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'email': ['jakub@status.im'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 0,
+}
+
+with DAG(
+    'example_task_decorator',
+    default_args=DEFAULT_ARGS,
+    start_date=days_ago(1),
+    tags=['example'],
+) as dag2:
+    task_start = DummyOperator(task_id='task_start')
+    task_mid_1 = DummyOperator(task_id='task_mid_1')
+    task_mid_2 = DummyOperator(task_id='task_mid_2')
+    task_mid_3 = DummyOperator(task_id='task_mid_3')
+    task_end = DummyOperator(task_id='task_end')
+    # Define dependencies
+    task_start >> [task_mid_2, task_mid_3, task_mid_4] >> task_end
