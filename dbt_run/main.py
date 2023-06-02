25adbd09c21e1cbfa6b9fa08248801c90b8fc89e
Alexis Pentori <alexis@status.im>
2023-06-02 15:25:56 +0200

    Updating DAG for dbt use
    Signed-off-by: Alexis Pentori <alexis@status.im>

--- dbt_run/main.py
+++ dbt_run/main.py
@@ -21,28 +21,30 @@ ARGS = {
     'retry_delay': timedelta(minutes=10),
 }
 
-with DAG('docker_dag', default_args=ARGS, schedule_interval="5 * * * *", catchup=False) as dag:
-    t1=BashOperator(
-    task_id='print_current_date',
-    bash_command='date'
+with DAG('dbt_execution', default_args=ARGS, schedule_interval=None, catchup=False) as dag:
+    task_test =  DockerOperator(
+	    task_id = 'dbt_test',
+    	image = 'dbt:test',
+	    network_mode='host',
+    	volumes=[
+                    '/docker/dbt-core/project/status-im/dbt-models:/usr/app', 
+                    '/docker/dbt-core/profile:/root/.dbt'
+                ],
+    	command='test',
+	    docker_url="unix://var/run/docker.sock",
+    	auto_remove=False,
     )
-    t2 = DockerOperator(
-    task_id='docker_command',
-    image='centos:latest',
-    api_version='auto',
-    auto_remove=True,
-    command="/bin/sleep 30",
-    docker_url="unix://var/run/docker.sock",
-    network_mode="bridge"
+    task_run =  DockerOperator(
+	    task_id = 'dbt_run',
+    	image = 'dbt:test',
+	    network_mode='host',
+    	volumes=[
+                    '/docker/dbt-core/project/status-im/dbt-models:/usr/app', 
+                    '/docker/dbt-core/profile:/root/.dbt'
+                ],
+    	command='run',
+	    docker_url="unix://var/run/docker.sock",
+    	auto_remove=False,
     )
-    t3 =  DockerOperator(
-	task_id = 'run_dbt',
-	image = 'dbt:test',
-	network_mode='host',
-	volumes=['/docker/dbt-core/project/status-im/dbt-models:/usr/app', '/docker/dbt-core/profile:/root/.dbt'],
-	command='run',
-	docker_url="unix://var/run/docker.sock",
-	auto_remove=False,
-    )
-    t1 >> t2  >> t3
+    task_test >> task_run
 
