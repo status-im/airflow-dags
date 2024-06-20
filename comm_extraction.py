568bcaa3cefb32f4d7c63db5293d293bfb73adc8
Alexis Pentori <alexis@status.im>
2024-06-20 09:46:41 +0200

    comm_extraction: make it run even in case of previous failure
      Create task_group to simplify dag
    
    Signed-off-by: Alexis Pentori <alexis@status.im>

--- comm_extraction.py
+++ comm_extraction.py
@@ -5,7 +5,7 @@ from datetime import datetime, timedelta
 import logging
 import time
 from airflow import DAG
-from airflow.decorators import dag, task
+from airflow.decorators import dag, task, task_group
 from airflow.operators.python import get_current_context
 from airflow.operators.bash_operator import BashOperator
 from airflow.utils.dates import days_ago
@@ -53,11 +53,22 @@ def wait_for_api():
     # https://developer.twitter.com/en/docs/twitter-api/tweets/lookup/api-reference/get-tweets#tab1
     time.sleep(900)
 
-
+# Task group to add a waiting time for the API to avoid reaching the API rate limit
+@task_group(group_id='fetch_twitter_info')
+def fetch_twitter_info(connection_id, airb_conn_name):
+    twitter_fetch = AirbyteTriggerSyncOperator(
+        task_id='airbyte_fetch_twittter_'+airb_conn_name,
+        airbyte_conn_id='airbyte_conn',
+        connection_id=connection_id,
+        asynchronous=False,
+        wait_seconds=3,
+        trigger_rule="all_done"
+    )
+    twitter_fetch >> wait_for_api()
+ 
 @dag(
     'comm_extraction', 
     default_args=ARGS, 
-    # Run  every 4 hours
     schedule_interval='0 */24  * * * '
 )
 def comm_extraction():
@@ -78,62 +89,27 @@ def comm_extraction():
         asynchronous=False,
         wait_seconds=3
     )
-    twitter_acid_info = AirbyteTriggerSyncOperator(
-        task_id='airbyte_fetch_twitter_acid_info',
-        airbyte_conn_id='airbyte_conn',
-        connection_id=connections_id['com_twitter_ac1d_info'],
-        asynchronous=False,
-        wait_seconds=3
-    )
-    twitter_nomos_tech = AirbyteTriggerSyncOperator(
-        task_id='airbyte_fetch_twitter_nomos_tech',
-        airbyte_conn_id='airbyte_conn',
-        connection_id=connections_id['com_twitter_nomos_tech'],
-        asynchronous=False,
-        wait_seconds=3
-    )
-    twitter_codex = AirbyteTriggerSyncOperator(
-        task_id='airbyte_fetch_twitter_codex',
-        airbyte_conn_id='airbyte_conn',
-        connection_id=connections_id['com_twitter_codex'],
-        asynchronous=False,
-        wait_seconds=3
-    )
-    twitter_logos = AirbyteTriggerSyncOperator(
-        task_id='airbyte_fetch_twitter_logos',
-        airbyte_conn_id='airbyte_conn',
-        connection_id=connections_id['com_twitter_logos'],
-        asynchronous=False,
-        wait_seconds=3
-    )
-    twitter_ethstatus = AirbyteTriggerSyncOperator(
-        task_id='airbyte_fetch_twitter_ethstatus',
-        airbyte_conn_id='airbyte_conn',
-        connection_id=connections_id['com_twitter_ethstatus'],
-        asynchronous=False,
-        wait_seconds=3
-    )
-    twitter_nimbus = AirbyteTriggerSyncOperator(
-        task_id='airbyte_fetch_twitter_nimbus',
-        airbyte_conn_id='airbyte_conn',
-        connection_id=connections_id['com_twitter_nimbus'],
-        asynchronous=False,
-        wait_seconds=3
-    )
-    twitter_waku = AirbyteTriggerSyncOperator(
-        task_id='airbyte_fetch_twitter_waku',
-        airbyte_conn_id='airbyte_conn',
-        connection_id=connections_id['com_twitter_waku'],
-        asynchronous=False,
-        wait_seconds=3
-    )
+
+    twitter_acid_info = fetch_twitter_info(connections_id['com_twitter_ac1d_info'], 'acid_info')
+    
+    twitter_nomos_tech = fetch_twitter_info(connections_id['com_twitter_nomos_tech'], 'nomos')
+     
+    twitter_codex = fetch_twitter_info(connections_id['com_twitter_codex'], 'codex')
+
+    twitter_logos = fetch_twitter_info(connections_id['com_twitter_logos'], 'logos')
+
+    twitter_ethstatus = fetch_twitter_info(connections_id['com_twitter_ethstatus'], 'status')
+    
+    twitter_nimbus = fetch_twitter_info(connections_id['com_twitter_nimbus'], 'nimbus')
+    
+    twitter_waku =  fetch_twitter_info(connections_id['com_twitter_waku'], 'waku')
 
     dbt_run_socials = BashOperator(
         task_id='dbt_run_models_social',
         bash_command='dbt run --profiles-dir /dbt --project-dir /dbt/dbt-models/ --select social'
     )
 
-    
-    connections_id >> [discord_fetcher, simplecast_fetch] >> twitter_acid_info >> wait_for_api() >> twitter_nomos_tech >> wait_for_api() >> twitter_codex >> wait_for_api() >> twitter_ethstatus >> wait_for_api() >> twitter_logos >> wait_for_api() >> twitter_waku >> wait_for_api() >> twitter_nimbus >> wait_for_api() >> dbt_run_socials
+    # Twitter connections have to be sequentially run to avoid API Rate Limits
+    connections_id >> [discord_fetcher, simplecast_fetch] >> twitter_acid_info  >> twitter_nomos_tech  >> twitter_codex  >> twitter_ethstatus  >> twitter_logos  >> twitter_waku  >> twitter_nimbus  >> dbt_run_socials
 
 comm_extraction()
